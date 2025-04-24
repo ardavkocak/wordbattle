94b760f32c58da6597e1630316a3de5c42c70b8e
ardavkocak <ardakocak1104@gmail.com>
2025-04-25 01:19:22 +0300

    anotherone

---
+++ WordBattle/WordBattle/backend/app/schemas.py
@@ -0,0 +1,20 @@
+# app/schemas.py
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
